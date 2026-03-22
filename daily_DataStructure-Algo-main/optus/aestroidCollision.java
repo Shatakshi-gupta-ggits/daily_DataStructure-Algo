e062248763bd9ec785e3e979e3df4d5fbfc5e34e
Shatakshi Gupta <shatakshig2005@gmail.com>
2026-03-22 23:06:33 +0530

    sha-returns

--- daily_DataStructure-Algo-main/optus/aestroidCollision.java
+++ daily_DataStructure-Algo-main/optus/aestroidCollision.java
@@ -1,5 +1,5 @@
 package optus;
 
-public enum aestroidCollision {
+public class aestroidCollision {
     
 }
---
+++ daily_DataStructure-Algo-main/optus/ifPrime.java
@@ -0,0 +1,10 @@
+package optus;
+
+public class ifPrime {
+
+
+    public static void main(String[] args){
+        
+    }
+    
+}
